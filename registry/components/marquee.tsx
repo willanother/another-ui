7791990a447604734a488c4d9bd25aefe4686114
chriswoodcn <chriswoodcn@aliyun.com>
2025-10-21 17:02:54 +0800

    feat: add marquee

---
+++ registry/components/marquee.tsx
@@ -0,0 +1,50 @@
+import { ComponentPropsWithoutRef } from 'react';
+
+import { cn } from '@/lib/utils';
+
+export function Marquee({
+  className,
+  reverse = false,
+  pauseOnHover = false,
+  children,
+  vertical = false,
+  repeat = 4,
+  ...props
+}: ComponentPropsWithoutRef<'div'> & {
+  className?: string;
+  reverse?: boolean;
+  pauseOnHover?: boolean;
+  children: React.ReactNode;
+  vertical?: boolean;
+  repeat?: number;
+}) {
+  return (
+    <div
+      {...props}
+      className={cn(
+        'group flex gap-(--gap) overflow-hidden p-2 [--duration:40s] [--gap:1rem]',
+        {
+          'flex-row': !vertical,
+          'flex-col': vertical,
+        },
+        className
+      )}
+    >
+      {Array(repeat)
+        .fill(0)
+        .map((_, i) => (
+          <div
+            key={i}
+            className={cn('flex shrink-0 justify-around gap-(--gap)', {
+              'animate-marquee flex-row': !vertical,
+              'animate-marquee-vertical flex-col': vertical,
+              'group-hover:paused': pauseOnHover,
+              '[animation-direction:reverse]': reverse
+            })}
+          >
+            {children}
+          </div>
+        ))}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,36 @@
+import { Marquee } from '$/registry/components/marquee';
 import PressEffect from '$/registry/components/press-effect';
 import SlideEffect from '$/registry/components/slide-effect';
 import { Button } from '@/components/ui/button';
+import MarqueeDemo from './marquee-demo';
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="w-full font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <PressEffect>
+        <Button>Button</Button>
+      </PressEffect>
+      <SlideEffect delay={0.5}>
         <PressEffect>
-          <Button>Button</Button>
+          <Button>Aschild</Button>
         </PressEffect>
-        <SlideEffect delay={0.5}>
-          <PressEffect>
-            <Button>Aschild</Button>
-          </PressEffect>
-        </SlideEffect>
-        <div className="bg-red-400 h-96"></div>
-        <div className="bg-amber-400 h-96"></div>
-        <div className="bg-red-400 h-96"></div>
-        <div className="bg-amber-400 h-96"></div>
-        <div className="bg-red-400 h-96"></div>
-        <div className="bg-amber-400 h-96"></div>
-        <SlideEffect delay={0.5} once={false} from="bottom">
-          <PressEffect>
-            <Button>Aschild</Button>
-          </PressEffect>
-        </SlideEffect>
-      </main>
+      </SlideEffect>
+      <div className="bg-red-400 h-96"></div>
+      <div className="bg-amber-400 h-96"></div>
+      <div className="bg-red-400 h-96"></div>
+      <div className="bg-amber-400 h-96"></div>
+      <div className="bg-red-400 h-96"></div>
+      <div className="bg-amber-400 h-96"></div>
+      <SlideEffect delay={0.5} once={false} from="bottom">
+        <PressEffect>
+          <Button>Aschild</Button>
+        </PressEffect>
+      </SlideEffect>
+      {/* <SlideEffect delay={0.5}> */}
+      <div className="overflow-hidden">
+        <MarqueeDemo />
+      </div>
+      {/* </SlideEffect> */}
     </div>
   );
 }
